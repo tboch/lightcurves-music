928d2b1167622a24ef7bd1785216786ba9860f8e
Thomas Boch <thomas.boch@gmail.com>
2018-11-13 01:42:39 +0100

    Sounds are playing at least!

--- website/js/app.js
+++ website/js/app.js
@@ -1,5 +1,5 @@
-var playStar = function (star) {
-    if (! star.params) {
+var updateLightCurve = function (star) {
+    if (!star) {
         return;
     }
     var phase_estimateArray = star.params.phase_estimate.slice(0, 48);
@@ -37,15 +37,19 @@ var playStar = function (star) {
 // initialize Aladin Lite
 var aladin = A.aladin('#aladin-lite-div', { target: 'sgr a*', fov: 10, cooFrame: 'galactic', survey: 'P/DSS2/red' });
 
-var currentStar = {
-    params: null,
-    id: null,
-    lcIndex: -1
-};
+// list of stars playing, indexed by source_id
+var musicians = [];
+var currentStar = null;
 // Set how we react when an object is clicked
 aladin.on('objectClicked', function (object) {
-    currentStar.lcIndex = 0;
+    if (object == null) {
+        currentStar = null;
+        return;
+    }
     var sourceId = object.data.source_id;
+    if (musicians.hasOwnProperty(sourceId)) {
+        return;
+    }
     var period = object.data.pf;
     var xhr = new XMLHttpRequest();
     /*
@@ -61,9 +65,17 @@ aladin.on('objectClicked', function (object) {
 
     xhr.onload = function () {
         if (xhr.status === 200) {
-            console.log(xhr.responseText);
-            currentStar.params = JSON.parse(xhr.responseText);
-            currentStar.id = sourceId;
+            currentStar = {
+                lcIndex: 0,
+                params: JSON.parse(xhr.responseText),
+                id: sourceId
+            };
+
+            currentStar.minMag = Math.min(...currentStar.params.mag_estimate);
+            currentStar.maxMag = Math.max(...currentStar.params.mag_estimate);
+            
+            
+
             //playStar(currentStar);
         }
         else if (xhr.status !== 200) {
@@ -73,28 +85,67 @@ aladin.on('objectClicked', function (object) {
     xhr.send();
 })
 
+// define custom draw function
+var drawFunction = function (source, canvasCtx, viewParams) {
+    canvasCtx.beginPath();
+    canvasCtx.arc(source.x, source.y, 6, 0, 2 * Math.PI, false);
+    canvasCtx.closePath();
+    // assign color based on period
+    var period = source.data.pf;
+    if (period > 3) {
+        period = 3;
+    }
+    canvasCtx.strokeStyle = '#' + Math.floor(0xffffff / (3 * period)).toString(16);
+
+    canvasCtx.lineWidth = 5;
+    canvasCtx.globalAlpha = 0.9,
+        canvasCtx.stroke();
+
+};
+
 // load catalogue with positions of Gaia stars for which we have light curves
+// draw function is too slow for that many sources :(
+//aladin.addCatalog(A.catalogFromURL('http://cds.unistra.fr/~boch/adass2018-hackathon/gaia-variable-sample.vot', { shape:drawFunction, onClick: 'showTable' }));
 aladin.addCatalog(A.catalogFromURL('http://cds.unistra.fr/~boch/adass2018-hackathon/gaia-variable-sample.vot', { color: 'red', onClick: 'showTable' }));
 
-var notes = ['C3', 'D#3', 'F3', 'G3', 'G#3', 'A#3', 'C4', 'D#4', 'F4', 'G4', 'G#4', 'A#4', 'C5', 'D#5'];
-// Tone JS
-Tone.Transport.bpm.value = 60;
 
+var notes = ['C3', 'D#3', 'F3', 'G3', 'G#3', 'A#3', 'C4', 'D#4', 'F4', 'G4', 'G#4', 'A#4', 'C5', 'D#5'];
+notes = ['A2', 'C3', 'D3', 'E3', 'G3', 'A3', 'C4', 'D4', 'E4', 'G4', 'A4']; // minor pentatonic
+// Tone JS
+Tone.Transport.bpm.value = 300;
+var synth = new Tone.Synth().toMaster();
+var piano = new Tone.PolySynth(4, Tone.Synth, {
+    "volume" : -1,
+    "oscillator" : {
+        "partials" : [1, 2, 5],
+    },
+    "portamento" : 0.005
+}).toMaster();
 var loop = new Tone.Loop(function (time) {
 
     Tone.Draw.schedule(function () {
-        playStar(currentStar);
-        console.log('update drawing');
+        updateLightCurve(currentStar);
+        //console.log('update drawing');
         //this callback is invoked from a requestAnimationFrame
         //and will be invoked close to AudioContext time
 
     }, time) //use AudioContext time of the event
 
-    currentStar.lcIndex++;
-    currentStar.lcIndex = currentStar.lcIndex%96;
-    //synth.triggerAttack('C4', '+0.05');
-    //triggered every 12th note. 
-    console.log(Tone.Transport.position);
-}, "48n").start(0);
+
+    if (currentStar) {
+        if (currentStar.lcIndex % 12 ==0) {
+            piano.triggerAttackRelease(['C3', 'E3', 'A3'], '1n');
+        }
+        currentStar.lcIndex++;
+        currentStar.lcIndex = currentStar.lcIndex % 96;
+        var i = currentStar.lcIndex % 48;
+        var noteIdx = Math.floor((notes.length - 1) * (currentStar.params.mag_estimate[i] - currentStar.minMag) / (currentStar.maxMag - currentStar.minMag));
+        var note = notes[notes.length - 1 - noteIdx];
+        synth.triggerAttackRelease(note, '+0.01');
+    }
+    //triggered every 48th mesure. 
+    //console.log(Tone.Transport.position);
+}, "12n").start(0);
 Tone.Transport.start();
+
 
